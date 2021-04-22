421df318504ccc7f0935fcc004cc0edd233b23bb
jaortisi6 <jaortisi@gmail.com>
2021-04-21 22:42:06 -0400

    Updates

---
+++ precache-manifest.a472b4c82857187f07d462982837bc7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de903e4a00e1f421393e0c71c933827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "027cfa8a017bdbc142c6",
+    "url": "/static/css/main.420d6436.chunk.css"
+  },
+  {
+    "revision": "3ddb980526134067f7d0",
+    "url": "/static/js/2.d6d2d680.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d6d2d680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027cfa8a017bdbc142c6",
+    "url": "/static/js/main.443c8d1f.chunk.js"
+  },
+  {
+    "revision": "5a658f61801743a1dc6d",
+    "url": "/static/js/runtime-main.794cdbbf.js"
+  }
+]);
